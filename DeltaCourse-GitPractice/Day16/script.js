625a3ab115794086fdb77b1e4345155fd2d04fe1
Ashish khadka <khadkaashish50@gmail.com>
2024-02-10 19:35:31 +0545

    latest

--- DeltaCourse-GitPractice/Day16/script.js
+++ DeltaCourse-GitPractice/Day16/script.js
@@ -7,3 +7,11 @@
 let password = prompt("set your password : ");
 let newPass = password.trim();
 console.log(newPass);
+
+let msg = "  help!"
+console.log(msg.trim().toUpperCase());
+
+let primary =["ram", "sita", "hari"]
+let secondary = ["Gita", "sita", "rita"]
+
+console.log(primary.concat(secondary));
