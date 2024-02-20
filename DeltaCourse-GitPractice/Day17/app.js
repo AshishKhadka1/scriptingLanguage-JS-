5b63e63fa0a3bb862e4adf2ba9c935883f88db6d
Ashish khadka <khadkaashish50@gmail.com>
2024-02-20 12:00:00 +0545

    latest

--- DeltaCourse-GitPractice/Day17/app.js
+++ DeltaCourse-GitPractice/Day17/app.js
@@ -50,3 +50,15 @@ for (let i = 1; i<= 5; i++){
  for (let i = 0; i<fruits.length; i++){
     console.log(i, fruits[i]);
  }
+
+
+//  nested loops with nested array
+let heroes = [ ["ironman", "spiderman"], ["superman", 'flash']]
+
+for(let i=0; i<heroes.length;i++){
+    console.log(`List #${i}`);
+    for(let j=0; j<heroes[i].length; j++){
+        console.log(heroes[i]);
+    }
+}
+
