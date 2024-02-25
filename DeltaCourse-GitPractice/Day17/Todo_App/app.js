0a145addb653c8b960f3a5ce4982289eb98b6486
Ashish khadka <khadkaashish50@gmail.com>
2024-02-25 14:45:49 +0545

    tarest

--- DeltaCourse-GitPractice/Day17/Todo_App/app.js
+++ DeltaCourse-GitPractice/Day17/Todo_App/app.js
@@ -1,20 +1,31 @@
-let todo = []; 
+let todo = [];
 
 let req = prompt("Please enter your request");
 
-while(true){
-    if(req == "quit"){
-        console.log("quitting app");
-        break;
-    }
+while (true) {
+  if (req == "qt") {
+    console.log("quitting app");
+    break;
+  }
 
-    if(req == "list"){
-        console.log("________________");
-        for(task of todo){
-            console.log(task);
-        }
-    console.log("_______________");
-    }else if(req == "add"){
-        prompt("Please enter your name :");
+  if (req == "li") {
+    console.log("________________");
+    for(let i = 0; i < todo.length; i++){
+        console.log(i, todo[i]);
     }
-}
+    console.log("_______________");
+  } else if (req == "add") {
+    let task = prompt("please enter the task you want to add");
+    todo.push(task);
+    console.log("Task added");
+  } else if(req == "del"){
+    let idx = prompt("please enter the task index");
+    todo.splice(idx, 1);
+    console.log("Task deleted");
+  }else{
+    console.log("Wrong request");
+  }
+
+  req = prompt("please enter your request")
+  }
+
