fe773e80fe5ab7152917b5e4a9ce737a07626224
Ashish khadka <khadkaashish50@gmail.com>
2024-03-10 21:53:42 +0545

    latest

--- DeltaCourse-GitPractice/Day19/basicFunction.js
+++ DeltaCourse-GitPractice/Day19/basicFunction.js
@@ -61,3 +61,30 @@ function printTable(n){
 }
 
 printTable(3);
+
+
+// return 
+function sum(a, b){
+    return a + b;
+}
+
+let s = sum(sum(1,999),111);
+console.log(s);
+
+
+// print number using function from 1 to n.
+function getSum(n) {
+    let sum = 0; // Initialize a variable to store the sum
+    
+    // Loop from 1 to n
+    for (let i = 1; i <= n; i++) {
+        sum += i; // Add the current number (i) to the sum
+    }
+    
+    return sum; // Return the sum
+}
+
+
+// create a function that returns the concatenation of all strings in an array.
+
+
